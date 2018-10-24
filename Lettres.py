86c451ac29421930c57989580c322d32b27badc9
azerluigi <38358708+azerluigi@users.noreply.github.com>
2018-10-24 18:19:12 +0300

    Update Lettres.py

--- Lettres.py
+++ Lettres.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def main():
+def Lettres():
     Lettres = {'E':1444, 'A':749, 'I':664, 'S':651, 'N':639, 'R':607,\
     'T':592, 'O':507, 'L':496, 'U':453, 'D':367, 'C':324,\
     'M':262, 'P':249, 'G':123, 'B':114, 'V':111, 'H':111,\
@@ -13,7 +13,3 @@ def main():
     for _ in range(10):
         print(lettres[randint(0, 8817)], end='')
         print(' ', end='')
-
-for _ in range(20):
-    main()
-    print()
