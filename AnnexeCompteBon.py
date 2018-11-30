3c773562a0cc420a045b864cf0d3010a049e400c
azerluigi <38358708+azerluigi@users.noreply.github.com>
2018-11-30 21:57:24 +0300

    Update AnnexeCompteBon.py

--- AnnexeCompteBon.py
+++ AnnexeCompteBon.py
@@ -3,13 +3,12 @@ from random import *
 def compteBon():
     nbTuiles = 6
     
-    suiteMsg = "" 
     resultat = randint(100, 1000)
 
     tuiles = [1,2,3,4,5,6,7,8,9,10]*2 + [25,50,75,100]
     shuffle(tuiles)
 
-    tuiles = tuiles[:6]
+    tuiles = tuiles[:nbTuiles]
 
     Msg = "Tuiles : " + " ".join(map(str,tuiles)) +  "\nA trouver : " + str(resultat) + "\n-"
     return Msg
